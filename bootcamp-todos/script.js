6f187c6c8fa6b050f127844594fb7cc1a9d244ed
Rachel Thompson <thompson.1657@gmail.com>
2020-11-21 13:18:06 -0600

    render todos

--- bootcamp-todos/script.js
+++ bootcamp-todos/script.js
@@ -4,3 +4,15 @@ var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+
+function renderTodos() {
+    for (var i=0; i < todos.length;i++){
+     var li = document.createElement('li')
+     
+     li.innerText = todos[i]
+     
+     todoList.appendChild(li)
+    }
+}
+
+renderTodos()
